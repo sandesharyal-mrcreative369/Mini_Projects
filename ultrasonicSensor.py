546bfc44b7b74e51fd184b1e9e52c12371fae7a1
sandesharyal-mrcreative369 <aryalsandesh2082@gmail.com>
2026-05-18 19:40:38 +0530

    Measure Echo Pulse

--- ultrasonicSensor.py
+++ ultrasonicSensor.py
@@ -27,6 +27,16 @@ try:
         GPIO.output(Trig_Pin, GPIO.LOW)
         time.sleep(0.00002)   #20 Microseconds
 
+
+        #Measure Echo Pulse
+        while GPIO.input(Echo_Pin) == GPIO.LOW:
+            pulse_start = time.time()
+
+        while GPIO.input(Trig_Pin) == GPIO.HIGH:
+            pulse_end = time.time()
+
+        pulse_duration = pulse_end - pulse_start
+
 except KeyboardInterrupt:
     print("Measurement stopped")
     GPIO.cleanup()
