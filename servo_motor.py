d57dc75a7aa322a97f65070cdb5c075eacaffa30
sandesharyal-mrcreative369 <aryalsandesh2082@gmail.com>
2026-05-18 20:01:42 +0530

    Adding rotation code servo motor

--- servo_motor.py
+++ servo_motor.py
@@ -11,4 +11,30 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(SERVO_PIN,GPIO.OUT)
 
 
+#PWM Setup
+pwm = GPIO.PWM(SERVO_PIN,50)  #servo motor works at 50Hz
+
+#Start PWM
+pwm.start(0)
+
+try:
+    while True:
+
+        # 0° -> 180°
+        for i in range(2,13):
+            pwm.ChangeDutyCycle(i)
+
+            time.sleep(0.4)
+
+        # 180° -> 0°
+        for j in range(12,1,-1):
+            pwm.ChangeDutyCycle(j)
+
+            time.sleep(0.5)
+
+
+except KeyboardInterrupt:
+    print("Measurement stopped")
+    GPIO.cleanup()
+
 
