1351fb43c2800f8dce4d2cc1a0cbc7c02fb05a67
sandesharyal-mrcreative369 <aryalsandesh2082@gmail.com>
2026-05-18 19:30:12 +0530

    Writing setup code for raspberry pi

---
+++ ultrasonicSensor.py
@@ -0,0 +1,17 @@
+#Imported Libraries
+import RPI.GPIO as GPIO
+import time
+
+#Define Pins
+Trig_Pin = 17
+Echo_Pin = 27
+
+
+#GPIO Setup
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(Trig_Pin, GPIO.IN)
+GPIO.setup(Echo_Pin, GPIO.OUT)
+
+#Ensure trigger pin is low when ultrasonic sensor is started
+GPIO.output(Trig_Pin, GPIO.LOW)
+time.sleep(2)
