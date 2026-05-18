7726eca85bf1a92c028dce9276786df10dd963a5
sandesharyal-mrcreative369 <aryalsandesh2082@gmail.com>
2026-05-18 19:37:14 +0530

    Generate Trigger Pulse

--- ultrasonicSensor.py
+++ ultrasonicSensor.py
@@ -18,7 +18,14 @@ time.sleep(2)
 
 
 try:
-    pass
+    while True:
+
+        #Generate Trigger Pulse
+        GPIO.output(Trig_Pin, GPIO.HIGH)
+        time.sleep(0.00005)  #50 Microseconds
+
+        GPIO.output(Trig_Pin, GPIO.LOW)
+        time.sleep(0.00002)   #20 Microseconds
 
 except KeyboardInterrupt:
     print("Measurement stopped")
