391b7f4fc22d5cd9f3ee01d84dd81af38d8fc4a2
sandesharyal-mrcreative369 <aryalsandesh2082@gmail.com>
2026-05-22 19:36:18 +0530

    adding raspberry pi side Server code

---
+++ server.py
@@ -0,0 +1,59 @@
+#This is the code for Raspberry pi as a server
+import socket
+import time
+from ultrasonic import *
+
+# -----------------------------------------
+# ULTRASONIC
+# -----------------------------------------
+TRIG_PIN = 12
+ECHO_PIN = 13
+
+initialize_ultrasonic(TRIG_PIN, ECHO_PIN)
+
+# -----------------------------------------
+# SOCKET SERVER
+# -----------------------------------------
+
+#To access Flask server from any network IP address
+HOST = '0.0.0.0'
+PORT = 5001
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server.bind((HOST, PORT))
+server.listen(1)
+
+print(f"Server started on port {PORT}. Waiting for laptop connection...")
+
+while True:
+    try:
+        conn, addr = server.accept()
+        print(f"Connected to: {addr}")
+
+        # -----------------------------------------
+        # SEND DISTANCE
+        while True:
+            pulse = generate_pulse(TRIG_PIN, ECHO_PIN)
+            distance = calculate_distance(pulse)
+
+            data = str(distance)
+
+            conn.send((data + "\n").encode())
+
+            time.sleep(0.1)
+
+    except (BrokenPipeError, ConnectionResetError):
+        print("Connection lost. Waiting for a new connection...")
+
+    except Exception as e:
+        print(f"Error occurred: {e}")
+
+    finally:
+
+        try:
+            conn.close()
+        except NameError:
+            pass
